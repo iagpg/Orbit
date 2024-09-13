e77e56a24e9c35ed2079bec67dacaeed09e094cb
iagopg <iago.pereirasp17@gmail.com>
2024-09-12 22:58:28 -0300

    biome fixed

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -4,4 +4,4 @@ import * as schema from './schema' // pegar todas as importações
 import { env } from '../env'
 
 export const client = postgres(env.DATABASE_URL)
-export const db = drizzle(client, { schema, logger: true }) // logger para visualizar as querys que fizermos no banco de dados
+    export const db = drizzle(client, { schema, logger: true }) // logger para visualizar as querys que fizermos no banco de dados
